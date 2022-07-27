9f0bf9f836e1d02e9434b6871e8dd84adf3a6c49
mauLovera <mauricioloveraedu@gmail.com>
2022-07-27 10:45:20 -0400

    working build

--- controllers/collections.js
+++ controllers/collections.js
@@ -29,6 +29,35 @@ function create(req, res) {
     })
 }
 
+function addBookmark(req, res) {
+  Collection.findById(req.params.id)
+    .then((collection) => {
+      collection.bookmarks.push(req.body)
+      collection.save().then((newBookmark) => {
+        res.json(newBookmark)
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ err: err.errmsg })
+    })
+}
+
+function deleteBookmark(req, res) {
+  console.log(req.body)
+  Collection.findById(req.params.id)
+    .then((collection) => {
+      collection.bookmarks[req.body.idx].remove()
+      collection.save().then((updatedCollection) => {
+        res.json(updatedCollection)
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ err: err.errmsg })
+    })
+}
+
 function deleteCollection(req, res) {
   Collection.findByIdAndDelete(req.params.id)
     .then((deletedCollection) => {
@@ -51,9 +80,4 @@ function update(req, res) {
     })
 }
 
-export {
-  index,
-  create,
-  deleteCollection as delete,
-  update,
-}
+export { index, create, addBookmark, deleteCollection as delete, deleteBookmark, update }
